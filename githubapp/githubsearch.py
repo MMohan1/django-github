76fc3d14ad75d5830b8c2417a0fd3a99af8d319d
MMohan1 <manmohansharma987@gmail.com>
2017-09-26 23:42:03 +0530

    Changes related to github save the data

--- githubapp/githubsearch.py
+++ githubapp/githubsearch.py
@@ -1,17 +1,21 @@
 import requests
+from django.contrib.auth.models import User
+from models import GitHub, Queries, Language, Location
+
+
 class gitHubApi():
     """
     """
+
     def __init__(self, request):
         self.request = request
         self.basic_search_url = "https://api.github.com/search/users"
 
-
     def make_the_search_query(self):
         """
         """
-        query_perms = {"location": "+location:",  "search_type": "+type:",\
-                       "fields":"+in:", "language":"+language:", "followers":"+followers:=>"}
+        query_perms = {"location": "+location:",  "search_type": "+type:",
+                       "fields": "+in:", "language": "+language:", "followers": "+followers:=>"}
         username = self.request.POST.get('user')
         self.basic_search_url += "?q=" + username
         for key, value in self.request.POST.iteritems():
@@ -30,12 +34,115 @@ class gitHubApi():
             return_dict["total"] = results_dict["total_count"]
             return_dict["query"] = self.basic_search_url
             return_dict["message"] = "Fatching the requests for Your given request is initiated...."
-        
+            self.get_all_the_results(results_dict)
         return return_dict
 
-    def get_the_results(self):
+    def get_the_results(self, search_url=None):
         """
         """
-        results = requests.get(self.basic_search_url)
+        if not search_url:
+            results = requests.get(self.basic_search_url)
+        else:
+            results = requests.get(search_url)
         return results
-        
+
+    def get_all_the_results(self, results_dict):
+        """
+        """
+        total = results_dict["total_count"]
+        location_obj, language_obj = None, None
+        query_obj = self.save_the_query(total)
+        if self.request.POST.get("location"):
+            location_obj = self.save_the_location(self.request.POST.get("location"))
+        if self.request.POST.get("language"):
+            language_obj = self.save_the_language(self.request.POST.get("language"))
+        self.save_the_results(results_dict["items"], query_obj, location_obj, language_obj)
+        pagination = 100
+        page = 2
+        while pagination < results_dict["total_count"]:
+            page_url = self.basic_search_url + "&page=" + str(page)
+            results = self.get_the_results(page_url)
+            if results.status_code == 200:
+                results_dict = results.json()
+                self.save_the_results(results_dict["items"], total)
+            else:
+                print "Something Went Wrong"
+            pagination += 100
+            page += 1
+
+    def complete_the_user_data(self, user_data):
+        """
+        """
+        followers = self.get_the_followers_count(user_data["followers_url"])
+        if followers != None:
+            user_data["followers"] = followers
+        repos = self.get_the_followers_count(user_data["repos_url"])
+        if repos != None:
+            user_data["repos"] = repos
+        return user_data
+
+    def save_the_results(self, results, query_obj, location_obj, language_obj):
+        """
+        """
+        for rec in results:
+            rec["query"] = self.basic_search_url
+            rec["github_id"] = rec.pop("id")
+            rec.pop("score")
+            user_data = self.complete_the_user_data(rec)
+            user = self.create_the_user(user_data)
+            user_data["user"] = user
+            user_data.pop("query")
+            github = self.save_the_github_data(user_data)
+            query_obj.github_set.add(github)
+            if location_obj:
+                location_obj.github_set.add(github)
+            if language_obj:
+                language_obj.github_set.add(github)
+
+    def save_the_github_data(self, user_data):
+        """
+        """
+        github_user, update = GitHub.objects.update_or_create(user=user_data["user"], defaults=user_data)
+        return github_user
+
+    def create_the_user(self, user_info):
+        """
+        """
+        username = user_info.pop("login")
+        user, created = User.objects.get_or_create(username=username, password=username)
+        return user
+
+    def get_the_followers_count(self, followers_url):
+        """
+        """
+        response = requests.get(followers_url)
+        if response.status_code == 200:
+            return len(response.json())
+        return None
+
+    def get_the_repos_count(self, repos_url):
+        """
+        """
+        response = requests.get(repos_url)
+        if response.status_code == 200:
+            return len(response.json())
+        return None
+
+    def save_the_location(self, location):
+        """
+        """
+        location, update = Location.objects.get_or_create(location=location)
+        return location
+
+    def save_the_query(self, total):
+        """
+        """
+        q = Queries(query=self.basic_search_url, total_results=total)
+        q.save()
+        return q
+
+    def save_the_language(self, language):
+        """
+        """
+        language, update = Language.objects.get_or_create(language=language)
+        return language
--- githubapp/models.py
+++ githubapp/models.py
@@ -1,3 +1,48 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Language(models.Model):
+    language = models.CharField(primary_key=True, max_length=1000)
+    created_date = models.DateTimeField(auto_now=True)
+
+
+class Location(models.Model):
+    location = models.CharField(primary_key=True, max_length=1000)
+    created_date = models.DateTimeField(auto_now=True)
+
+
+class Queries(models.Model):
+    query = models.TextField(primary_key=True)
+    total_results = models.IntegerField()
+    created_date = models.DateTimeField(auto_now=True)
+
+
+class GitHub(models.Model):
+    user = models.ForeignKey(User)
+    github_id = models.IntegerField()
+    avatar_url = models.TextField()
+    gravatar_id = models.TextField()
+    url = models.TextField()
+    html_url = models.TextField()
+    followers_url = models.TextField()
+    following_url = models.TextField()
+    gists_url = models.TextField()
+    starred_url = models.TextField()
+    subscriptions_url = models.TextField()
+    organizations_url = models.TextField()
+    repos_url = models.TextField()
+    events_url = models.TextField()
+    received_events_url = models.TextField()
+    type = models.TextField()
+    site_admin = models.TextField()
+    location = models.ForeignKey("Location", null=True)
+    language = models.ForeignKey("Language", null=True)
+    followers = models.IntegerField()
+    repos = models.IntegerField()
+    query = models.ForeignKey("Queries", null=True)
+    creation_date = models.DateTimeField(auto_now=True)
+
