45135b522545244a4333efd4ebdf48a4b08d4196
MMohan1 <manmohansharma987@gmail.com>
2017-09-27 23:14:53 +0530

    Report Changes

--- githubapp/admin.py
+++ githubapp/admin.py
@@ -1,27 +1,25 @@
 from django.contrib import admin
-from .models import GitHub
+from .models import GitHub, Queries
+from rangefilter.filter import DateRangeFilter
 
-from operator import or_
-from django.db.models import Q
-
-class MyAdmin(admin.ModelAdmin):
-    
-    def get_search_results(self, request, queryset, search_term):
-        queryset, use_distinct = super(MyAdmin, self).get_search_results(
-                                               request, queryset, search_term)
-        search_words = search_term.split()
-        if search_words:
-            q_objects = [Q(**{field + '__icontains': word})
-                                for field in self.search_fields
-                                for word in search_words]
-            queryset |= self.model.objects.filter(reduce(or_, q_objects))
-        return queryset, use_distinct
 
 @admin.register(GitHub)
 class GitHubAdmin(admin.ModelAdmin):
     fields = ('user', 'github_id', 'followers')
-    list_display = ['admin_thumbnail', 'user', "email",'creation_date', "followers", "public_repos", "location"]
-    search_fields = ("email", 'created_date')
-    list_filter = ('public_repos',)
+    list_display = ['admin_thumbnail', 'user', "email", 'creation_date', "followers", "public_repos", "location"]
+    search_fields = ("email",)
+    list_filter = (('creation_date', DateRangeFilter), 'public_repos', "location", "followers")
 
+
+@admin.register(Queries)
+class ReportAdmin(admin.ModelAdmin):
+    change_list_template = "admin/github_report.html"
+    date_hierarchy = "created_date"
+    list_filter = (('created_date', DateRangeFilter),)
     
+    def changelist_view(self, request, extra_context=None):
+        response = super(ReportAdmin, self).changelist_view(request, extra_context=extra_context,)
+        results_dict = [{"category": "User", "today": 10, "week": 20, "month": 70},
+                        {"category": "Queries", "today": 10, "week": 20, "month": 70}]
+        response.context_data['summary'] = results_dict
+        return response
